454fd839c4dd3dbf2e42387ffb19e52b334d1e68
Inareous <evil_septian@yahoo.co.id>
2018-08-04 12:56:41 +0700

    Add project files.

---
+++ Minimal CS Manga Reader/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Minimal_CS_Manga_Reader
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
