18f555d4375fa61225abfa92121fe65e5a695822
Inareous <septian.wijaya@mail.ugm.ac.id>
2020-07-11 18:00:35 +0700

    fix invalid reflection in settings because itemssource bind is not initialized first.

--- Minimal CS Manga Reader/Views/SettingView.xaml.cs
+++ Minimal CS Manga Reader/Views/SettingView.xaml.cs
@@ -18,12 +18,12 @@ namespace Minimal_CS_Manga_Reader
                 this.BindCommand(ViewModel, vm => vm.Close, view => view.Close).DisposeWith(d);
                 this.BindCommand(ViewModel, vm => vm.Save, view => view.Save).DisposeWith(d);
                 this.BindCommand(ViewModel, vm => vm.SaveAndRefresh, view => view.SaveAndRefresh).DisposeWith(d);
-                this.Bind(ViewModel, vm => vm.SelectedPixelOffsetMode, view => view.PixelOffsetMode.SelectedItem).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.PixelOffsetMode, view => view.PixelOffsetMode.ItemsSource).DisposeWith(d);
-                this.Bind(ViewModel, vm => vm.SelectedInterpolationMode, view => view.InterpolationMode.SelectedItem).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.InterpolationMode, view => view.InterpolationMode.ItemsSource).DisposeWith(d);
-                this.Bind(ViewModel, vm => vm.SelectedSmoothingMode, view => view.SmoothingMode.SelectedItem).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.SmoothingMode, view => view.SmoothingMode.ItemsSource).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.SelectedPixelOffsetMode, view => view.PixelOffsetMode.SelectedItem).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.SelectedInterpolationMode, view => view.InterpolationMode.SelectedItem).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.SelectedSmoothingMode, view => view.SmoothingMode.SelectedItem).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.ContextIntegrated, view => view.ContextIntegrated.IsChecked).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.FitImagesToScreen, view => view.FitImagesToScreen.IsChecked).DisposeWith(d);
                 this.Bind(ViewModel, vm => vm.OpenChapterOnLoad, view => view.OpenChapterOnLoadList.SelectedItem).DisposeWith(d);
